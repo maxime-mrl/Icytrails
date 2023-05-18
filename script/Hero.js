a3dc547e729241defa5b437e2c42654941d7b086
maxime-mrl <maxitrompette@ymail.com>
2023-05-18 14:10:33 +0200

    started to work on collisions

--- script/Hero.js
+++ script/Hero.js
@@ -24,8 +24,8 @@ export default class Hero {
     update = (delay) => {
         this.updatePos(delay);
         this.vCollision();
+        this.collisionBlock();
 
-        console.log(this.vel.y)
         this.pos.x += this.vel.xAbs * delay/1000 * this.vel.mdir;
         this.draw()
     }
@@ -50,6 +50,28 @@ export default class Hero {
             this.pos.y = 0
             this.vel.y = 0
         }
+    }
+
+    collisionBlock = () => {
+        this.world.level.forEach((row, blockY) => {
+            row.forEach((type, blockX) => {
+                if (type != "a") return;
+                if (
+                    ( // x collision
+                        blockX >= this.pos.x && blockX < this.pos.x + 1 ||
+                        blockX < this.pos.x && blockX + 1 > this.pos.x
+                    ) &&
+                    (// y collision
+                        blockY >= this.pos.y && blockY < this.pos.y + 1
+                    )
+                    ) 
+                {
+                    const {x:rx, y:ry} = this.renderer.calculateCoords({x: blockX,y:blockY});
+                    this.ctx.fillStyle = "red";
+                    this.ctx.fillRect(rx, ry, this.renderer.blockSize, this.renderer.blockSize)
+                }
+            })
+        });
     }
 
     draw = () => {
