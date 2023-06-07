2ac9ff7e62ed3b5d8fe2793f183c28dbdb7de8c7
maxime-mrl <maxitrompette@ymail.com>
2023-06-07 22:56:21 +0200

    404 hero now talk so it's funnier

--- script/404.js
+++ script/404.js
@@ -1,5 +1,18 @@
 import Renderer from "./classes/Renderer.js";
 import Hero from "./classes/Hero.js";
+const texts = [
+    "Where am I?",
+    "Fck, I'm lost",
+    "What is 404?",
+    "Any way out?",
+    "Someone here?",
+    "Arggh...",
+    "Still here?",
+    "Idk, go home",
+    "something's wrong",
+    "42 is the answer",
+    "Or is it?",
+]
 
 fetch('/script/404.json') // get 404 "level"
     .then(resp => resp.json())
@@ -11,10 +24,12 @@ class World {
         this.ctx = this.canvas.getContext("2d");
         this.level = level;
         this.texts = level.text;
-        console.log(this.texts)
         this.translate = { x: 0, y: 0 };
         this.memSpeed = 0;
         this.moving = true;
+        this.currentText = 0;
+        this.chatting = true;
+        setTimeout(() => this.chatting = false, 2500);
 
         // level read and initialization
         this.max = Math.max.apply(0, this.level.fg.map(elem => elem.x)); // level x border
@@ -38,10 +53,36 @@ class World {
         this.drawText();
         // update player
         this.player.update(delay);
+        // chat
+        if (this.chatting) this.playerTalk();
+        if (!this.chatting && Math.random() > 0.9993) {
+            this.chatting = true;
+            this.currentText++
+            if (this.currentText >= texts.length) this.currentText = 0;
+            setTimeout(() => this.chatting = false, 2000 + Math.random() * 1000);
+        }
         // simulate movement
         this.updateMovement()
     }
 
+    playerTalk = () => {
+        const text = texts[this.currentText];
+        this.ctx.font = `500 16px atma`;
+        const txtSize = this.ctx.measureText(text).width;
+        const coords = this.renderer.calculateCoords({
+            x:this.player.pos.x + this.player.hitBox.width + 0.2,
+            y:this.player.pos.y + this.player.hitBox.height - 1
+        });
+        // draw bubles
+        this.ctx.fillStyle = "#022e3c99";
+        this.drawEllipse(coords.x, coords.y, 6);
+        this.drawEllipse(coords.x + + 15, coords.y - 15, 10);
+        this.drawEllipse(coords.x + 50, coords.y - txtSize * 0.6, txtSize * 0.7);
+        // draw text
+        this.ctx.fillStyle = "#F3EFF5ee";
+        this.ctx.fillText(text, coords.x + 50 - txtSize * 0.5, coords.y - txtSize*0.55);
+    }
+    
     updateMovement = () => {
         if (this.player.vel.xAbs < this.memSpeed / 2 && !this.player.jumping) this.player.jump(); // auto jump
         // random jump and stop
@@ -76,5 +117,12 @@ class World {
         })
         
         this.ctx.restore()
+    }
+    
+    drawEllipse = (x, y, size) => {
+        this.ctx.beginPath();
+        this.ctx.ellipse(x , y, size, Math.min(size * 0.45, 40), 0, 0, 2*Math.PI);
+        this.ctx.fill();
+        this.ctx.closePath();
     }
 }
