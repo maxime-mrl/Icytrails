9cc4d6768dfdb417efea2e42c60efc7182729f33
maxime-mrl <maxitrompette@ymail.com>
2023-06-07 12:05:11 +0200

    level list good

--- script/app.js
+++ script/app.js
@@ -1,3 +1,12 @@
 const editor = document.getElementById("editor-container");
-
 const toggleEditor = () => editor.classList.toggle("modal-oppened");
+
+document.querySelectorAll(".level").forEach(card => {
+    console.log(card)
+    card.onmousemove = e => {
+        const { currentTarget: target } = e;
+        const rect = target.getBoundingClientRect();
+        target.style.setProperty("--mouse-x", `${e.clientX - rect.left}px`);
+        target.style.setProperty("--mouse-y", `${e.clientY - rect.top}px`);
+    }
+})
